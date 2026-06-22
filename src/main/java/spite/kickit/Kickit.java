340123630374cb56711e6b7d5e1ad065dc001882
Your Name <you@example.com>
2026-06-22 11:03:11 +0800

    添加mek剔除

---
+++ src/main/java/spite/kickit/Kickit.java
@@ -0,0 +1,13 @@
+package spite.kickit;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(Kickit.MODID)
+public class Kickit {
+    public static final String MODID = "kickit";
+    public Kickit(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
